f90a5e608a1c8d37b77947e69c7c33cc43ba7804
Sarah Micallef <sarah.cassar92@gmail.com>
2018-05-01 11:56:29 +0200

    Fixed validator pattern & added logs

--- src/main/java/com/excercise/tripaths/parser/validation/TextTriangleValidator.java
+++ src/main/java/com/excercise/tripaths/parser/validation/TextTriangleValidator.java
@@ -1,5 +1,6 @@
 package com.excercise.tripaths.parser.validation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
 import javax.validation.ConstraintValidator;
@@ -10,11 +11,13 @@ import java.util.regex.Pattern;
 /**
  * {@link TextTriangle} validator.
  */
+@Slf4j
 public class TextTriangleValidator implements ConstraintValidator<TextTriangle, List<String>> {
 
-    private static final Pattern VALID_TEXT_TRIANGLE_ROW_FORMAT = Pattern.compile("^(\\d)+(\\s.(\\d)+)*$");
+    private static final Pattern VALID_TEXT_TRIANGLE_ROW_FORMAT = Pattern.compile("^(\\d)+(\\s(\\d)+)*$");
     @Override
     public boolean isValid(final List<String> value, final ConstraintValidatorContext context) {
+        log.trace("Checking text triangle validity.");
 
         if (value == null) {
             return true;
@@ -24,13 +27,20 @@ public class TextTriangleValidator implements ConstraintValidator<TextTriangle, 
         for (final String row : value) {
 
             final int expectedNumberOfDigitsInRow = rowIndex;
-            if (!VALID_TEXT_TRIANGLE_ROW_FORMAT.matcher(row).matches() || StringUtils.delimitedListToStringArray(row, " ").length != expectedNumberOfDigitsInRow) {
+            if (!VALID_TEXT_TRIANGLE_ROW_FORMAT.matcher(row).matches()) {
+                log.error("Row {} not valid as it does not match pattern {}", row, VALID_TEXT_TRIANGLE_ROW_FORMAT.toString());
+                return false;
+            }
+
+            if (StringUtils.delimitedListToStringArray(row, " ").length != expectedNumberOfDigitsInRow) {
+                log.error("Row {} not valid as it does not contain the expected number of digits [{}].", row, expectedNumberOfDigitsInRow);
                 return false;
             }
 
             rowIndex++;
         }
 
+        log.debug("Text triangle valid.");
         return true;
     }
 
